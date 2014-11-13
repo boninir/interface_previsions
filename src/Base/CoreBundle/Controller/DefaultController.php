9dee41d3b1db5b2d9b84ff55d03b65505406bb9b
Raphael Bonini <raphael.eros.bonini@gmail.com>
2014-11-13 17:27:03 +0100

    commit de la structure de base (mise en place des templates de chaque page), intégration partielle de bootstrap (css et js ok, les glyphicons posent encore problème)

--- src/Base/CoreBundle/Controller/DefaultController.php
+++ src/Base/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BaseCoreBundle:Default:index.html.twig');
+        return $this->render('BaseCoreBundle:Core:content.html.twig');
+    }
+
+    public function meteoAction()
+    {
+        return $this->render('BaseCoreBundle:Core:meteo.html.twig');
+    }
+
+    public function statuscodeAction()
+    {
+        return $this->render('BaseCoreBundle:Core:statuscode.html.twig');
+    }
+
+    public function erreurAction()
+    {
+        return $this->render('BaseCoreBundle:Core:erreur.html.twig');
+    }
+
+    public function bugAction()
+    {
+        return $this->render('BaseCoreBundle:Core:bug.html.twig');
     }
 }
