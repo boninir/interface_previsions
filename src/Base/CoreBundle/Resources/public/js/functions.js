3e056f4731ff48bdb7992bc77915fc6cb7806ed6
Raphael Bonini <raphael.eros.bonini@gmail.com>
2015-02-02 18:07:30 +0100

    mise à jour des scripts js, affichage de la fenetre modale sur la page des status codes.

---
+++ src/Base/CoreBundle/Resources/public/js/functions.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    // permet d'afficher les datepicker de l'interface
+                $(".datepicker").datepicker({dateFormat: 'dd/mm/yy',
+                                                        firstDay:1
+                                                    }).attr("readonly","readonly");
+
+
+                // permet de gérer les checkbox en cascade du menu latéral gauche (lorsqu'on
+                // coche un parc, toutes les turbines sont cochées)
+                $(".acidjs-css3-treeview").delegate("label input:checkbox", "change", function() {
+                    var
+                        checkbox = $(this),
+                        nestedList = checkbox.parent().next().next(),
+                        selectNestedListCheckbox = nestedList.find("label:not([for]) input:checkbox");
+                 
+                    if(checkbox.is(":checked")) {
+                        return selectNestedListCheckbox.prop("checked", true);
+                    }
+                    selectNestedListCheckbox.prop("checked", false);
+                });
+    $('#myModal').modal('show');
+});
